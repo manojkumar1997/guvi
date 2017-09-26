9fad5ff19dd92de0de76146da48c26f90fb9c508
manojkumar1997 <cutemanoj197@gmail.com>
2017-09-26 08:49:11 -0700

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if(isalpha(a))
+{
+printf("the character is alphapet");
+}
+else
+{
+ printf("the character is not alpha");
+ }
+ return 0;
+ }
